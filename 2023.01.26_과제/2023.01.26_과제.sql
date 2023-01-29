101b34619d041d72a0f6f6398edcb42a28b58579
filson1205 <sondongfil1205@gmail.com>
2023-01-30 00:35:59 +0900

    문제풀기

--- 2023.01.26_과제/2023.01.26_과제.sql
+++ 2023.01.26_과제/2023.01.26_과제.sql
@@ -1,3 +1,5 @@
+--[Basic SELECT]
+
 --1. 춘 기술대학교의 학과 이름과 계열을 표시하시오. 단, 출력 헤더는 "학과 명", "계열"
 --으로 표시하도록 한다.
 SELECT
@@ -75,6 +77,25 @@ SELECT
   WHERE ENTRANCE_DATE LIKE '02%'
     AND STUDENT_ADDRESS LIKE '전주%'
     AND ABSENCE_YN = 'N';
+
+--[Additional SELECT - 함수]
+
+--1. 영어영문학과(학과코드 002) 학생들의 학번과 이름, 입학 년도를 입학 년도가 빠른
+--순으로 표시하는 SQL 문장을 작성하시오.( 단, 헤더는 "학번", "이름", "입학년도" 가
+--표시되도록 핚다.)
+SELECT
+       STUDENT_NO 학번
+     , STUDENT_NAME 이름
+     , ENTRANCE_DATE 입학년도
+  FROM TB_STUDENT
+  WHERE DEPARTMENT_NO = '002'
+  ORDER BY ENTRANCE_DATE;
+  
+--2. 춘 기술대학교의 교수 중 이름이 세 글자가 아닌 교수가 핚 명 있다고 핚다. 그 교수의
+--이름과 주민번호를 화면에 출력하는 SQL 문장을 작성해 보자. (* 이때 올바르게 작성핚 SQL
+--문장의 결과 값이 예상과 다르게 나올 수 있다. 원인이 무엇일지 생각해볼 것)
+
+
 
  
 
